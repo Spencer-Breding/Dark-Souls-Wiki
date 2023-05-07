9a589583a0010f403ddd6cbfa3db7a9787d80668
Spencer-Breding <Spencerbreding@gmail.com>
2023-05-07 09:43:56 -0700

    Optimized some CSS

--- components/ArmorTableRow.js
+++ components/ArmorTableRow.js
@@ -14,92 +14,92 @@ export default function ArmorTableRow(props) {
             <div className={styles.table_item_info }>
                 <div className={styles.table_item_column }>
                     <text>{props.durability}</text>
-                    <img className={styles.durability_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_other_dura.png" />
-                    <div className={styles.durability_container}>
+                    <img id={styles.durability_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_other_dura.png" />
+                    <div id={styles.durability_container} className={styles.table_dropdown}>
                         <text>Durability</text>
                     </div>
                 </div>
                 <div className={styles.table_item_column}>
                     <text>{props.weight}</text>
-                    <img className={styles.weight_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_other_weig.png" />
-                    <div className={styles.weight_container}>
+                    <img id={styles.weight_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_other_weig.png" />
+                    <div id={styles.weight_container} className={styles.table_dropdown}>
                         <text>Weight</text>
                     </div>
                 </div>
                 <div className={styles.table_item_column}>
                     <text>{props.regular}</text>
-                    <img className={styles.regular_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_phy.png" />
-                    <div className={styles.regular_container}>
+                    <img id={styles.regular_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_phy.png" />
+                    <div id={styles.regular_container} className={styles.table_dropdown }>
                         <text>Regular Defense</text>
                     </div>
                 </div>
                 <div className={styles.table_item_column}>
                     <text>{props.strike}</text>
-                    <img className={styles.strike_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_strike.png" />
-                    <div className={styles.strike_container}>
+                    <img id={styles.strike_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_strike.png" />
+                    <div id={styles.strike_container} className={styles.table_dropdown}>
                         <text>Strike Defense</text>
                     </div>
                 </div>
                 <div className={styles.table_item_column}>
                     <text>{props.slash}</text>
-                    <img className={styles.slash_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_slash.png" />
-                    <div className={styles.slash_container}>
+                    <img id={styles.slash_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_slash.png" />
+                    <div id={styles.slash_container} className={styles.table_dropdown}>
                         <text>Slash Defense</text>
                     </div>
                 </div>
                 <div className={styles.table_item_column}>
                     <text>{props.thrust}</text>
-                    <img className={styles.thrust_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_thrust.png" />
-                    <div className={styles.thrust_container}>
+                    <img id={styles.thrust_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_thrust.png" />
+                    <div id={styles.thrust_container} className={styles.table_dropdown}>
                         <text>Thrust Defense</text>
                     </div>
                 </div>
                 <div className={styles.table_item_column}>
                     <text>{props.magic}</text>
-                    <img className={styles.magic_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_magi.png" />
-                    <div className={styles.magic_container}>
+                    <img id={styles.magic_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_magi.png" />
+                    <div id={styles.magic_container} className={styles.table_dropdown}>
                         <text>Magic Defense</text>
                     </div>
                 </div>
                 <div className={styles.table_item_column}>
                     <text>{props.fire}</text>
-                    <img className={styles.fire_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_fire.png" />
-                    <div className={styles.fire_container}>
+                    <img id={styles.fire_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_fire.png" />
+                    <div id={styles.fire_container} className={styles.table_dropdown}>
                         <text>Fire Defense</text>
                     </div>
                 </div>
                 <div className={styles.table_item_column}>
                     <text>{props.lightning}</text>
-                    <img className={styles.lightning_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_lightn.png" />
-                    <div className={styles.lightning_container}>
+                    <img id={styles.lightning_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_prot_lightn.png" />
+                    <div id={styles.lightning_container} className={styles.table_dropdown}>
                         <text>Lightning Defense</text>
                     </div>
                 </div>
                 <div className={styles.table_item_column}>
                     <text>{props.bleed}</text>
-                    <img className={styles.bleed_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_res_bleed.png" />
-                    <div className={styles.bleed_container}>
+                    <img id={styles.bleed_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_res_bleed.png" />
+                    <div id={styles.bleed_container} className={styles.table_dropdown}>
                         <text>Bleed Resistance</text>
                     </div>
                 </div>
                 <div className={styles.table_item_column}>
                     <text>{props.poison}</text>
-                    <img className={styles.poison_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_res_poison.png" />
-                    <div className={styles.poison_container}>
+                    <img id={styles.poison_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_res_poison.png" />
+                    <div id={styles.poison_container} className={styles.table_dropdown}>
                         <text>Poison Resistance</text>
                     </div>
                 </div>
                 <div className={styles.table_item_column}>
                     <text>{props.curse}</text>
-                    <img className={styles.curse_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_res_curse.png" />
-                    <div className={styles.curse_container}>
+                    <img id={styles.curse_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_res_curse.png" />
+                    <div id={styles.curse_container} className={styles.table_dropdown}>
                         <text>Curse Resistance</text>
                     </div>
                 </div>
                 <div className={styles.table_item_column}>
                     <text>{props.poise}</text>
-                    <img className={styles.poise_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_res_poise.png" />
-                    <div className={styles.poise_container}>
+                    <img id={styles.poise_icon} src="https://darksouls.wiki.fextralife.com/file/Dark-Souls/icon_res_poise.png" />
+                    <div id={styles.poise_container} className={styles.table_dropdown}>
                         <text>Poise</text>
                     </div>
                 </div>
