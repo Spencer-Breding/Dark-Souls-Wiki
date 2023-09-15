f789fd1ec72195d6bb29ee74022d98d17c9c1afd
Spencer-Breding <Spencerbreding@gmail.com>
2023-09-14 20:22:03 -0700

    Changed videos to lazy loading

--- components/VideoGallery.js
+++ components/VideoGallery.js
@@ -6,7 +6,7 @@ export default function VideoGallery({ items, setCurrentIndex }) {
         <div className={styles.gallery}>
             {items.map((item, index) => (
                 <div key={index} className={styles.gallery_item} onClick={() => setCurrentIndex(index)}>
-                    <Iframe src={item.source} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen />
+                    <Iframe src={item.source} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen loading="lazy"/>
                     <p>{item.description}</p>
                 </div>
             ))}
