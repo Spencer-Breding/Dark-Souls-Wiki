fb3553f5c535aead9cd87b6dab12207969e4826a
Spencer-Breding <Spencerbreding@gmail.com>
2024-05-20 10:10:58 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,57 +1,58 @@
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
+    output: 'export',  // This is the new property you need to add
     assetPrefix: isProd ? '/Dark-Souls-Wiki/' : '',
     images: {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'www.pngmart.com'
+                hostname: 'www.pngmart.com',
             },
             {
                 protocol: 'http',
-                hostname: '**.imgur.com'
+                hostname: '**.imgur.com',
             },
             {
                 protocol: 'https',
-                hostname: '**.imgur.com'
+                hostname: '**.imgur.com',
             },
             {
                 protocol: 'https',
-                hostname: '**.gematsu.com'
+                hostname: '**.gematsu.com',
             },
             {
                 protocol: 'https',
-                hostname: '**.pinimg.com'
+                hostname: '**.pinimg.com',
             },
             {
                 protocol: 'https',
-                hostname: 'conceptartworld.com'
+                hostname: 'conceptartworld.com',
             },
             {
                 protocol: 'https',
-                hostname: '**.media-amazon.com'
+                hostname: '**.media-amazon.com',
             },
             {
                 protocol: 'https',
-                hostname: 'image.api.playstation.com'
+                hostname: 'image.api.playstation.com',
             },
             {
                 protocol: 'https',
-                hostname: '**.creativeuncut.com'
+                hostname: '**.creativeuncut.com',
             },
             {
                 protocol: 'https',
-                hostname: 'cdn.cloudflare.steamstatic.com'
+                hostname: 'cdn.cloudflare.steamstatic.com',
             },
             {
                 protocol: 'https',
-                hostname: "darksouls.wiki.fextralife.com"
+                hostname: "darksouls.wiki.fextralife.com",
             },
             {
                 protocol: 'https',
-                hostname: "static.wikia.nocookie.net"
+                hostname: "static.wikia.nocookie.net",
             },
         ],
     },
-}
+};
